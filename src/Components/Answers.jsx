39a886097f913aab4a549f381bb770fdd9c5b652
Anthony Liang <anthonyliang3@gmail.com>
2022-08-25 15:23:25 -0400

    Answers added

---
+++ src/Components/Answers.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Answers = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+export default Answers;
